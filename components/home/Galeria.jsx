56c03d98314df93c160263e9510bb2acdee8accd
Iuri Marques <iurimarcianom@hotmail.com>
2025-08-11 16:00:10 +0100

    Update image gallery components for improved layout and functionality

--- components/home/Galeria.jsx
+++ components/home/Galeria.jsx
@@ -38,8 +38,8 @@ export default function Galeria() {
                 
                 {
                     images.length > 10 && (
-                        <div id="SeeMoreButton" className={` ${showAll ? 'bottom-0' : 'absolute bottom-50 h-50'} inset-x-0  px-[35px]`}> {/*Botão 'Ver Mais'*/}
-                            <div className={`flex flex-col justify-center items-center bg-gradient-to-b from-white/1 from-10% via-white via-70% to-white to-100% w-full ${showAll ? 'h-10': 'h-100'}`}>
+                        <div id="SeeMoreButton" className={` ${showAll ? 'bottom-0' : 'absolute md:bottom-5 bottom-30 h-50'} inset-x-0  px-[35px]`}> {/*Botão 'Ver Mais'*/}
+                            <div className={`flex flex-col justify-center items-center bg-gradient-to-b from-white/1 from-10% via-white via-70% to-white to-100% w-full ${showAll ? 'h-10': 'md:h-50 h-80'}`}>
                                 <div onClick={toggleShowAll} className="flex flex-col items-center cursor-pointer hover:scale-105">
 
                                     {showAll ? 
--- components/home/ImagemGaleria.jsx
+++ components/home/ImagemGaleria.jsx
@@ -5,6 +5,8 @@ export default function ImagemGaleria(props) {
     const [isOpen, setIsOpen] = useState(false);// Estado para controlar a exibição das fotos
     const [isLandscape, setIsLandscape] = useState(false);
 
+    const downloadUrl = props.url.replace(`/upload/`, `/upload/fl_attachment:${props.id}/`);
+
     useEffect(() => {
         if (props.width && props.height) {
         setIsLandscape(props.width > props.height);
@@ -26,17 +28,19 @@ export default function ImagemGaleria(props) {
         };
     }, [isOpen]);
     
+    console.log("URL:", downloadUrl);
     return (
-        <div className="overflow-hidden w-full">
+        <div className="overflow-hidden w-full h-fit place-self-center">
             <Image
                 src={props.url}
                 alt={props.id}
                 width={400}
                 height={400}
-                className="shadow w-full h-full object-contain hover:cursor-pointer hover:brightness-70"
+                className=" w-full h-full object-contain hover:cursor-pointer hover:brightness-70"
                 onClick={() => setIsOpen(true)}
             />
 
+            {/* Modal para exibir a imagem em tamanho maior */}
             {
                 isOpen && 
                 (<div className="fixed top-0 right-0 w-full h-screen bg-black/80 z-99 flex flex-col justify-center items-center">
@@ -44,7 +48,7 @@ export default function ImagemGaleria(props) {
 
                     <Image width={300} height={300} src={props.url} alt={props.id} className={`object-contain h-full ${isLandscape ? "rotate-90 md:rotate-0" : ""} `}/>
                     
-                    <a download href={props.url} className="absolute bottom-8 p-2 bg-[#EAEAEA] hover:opacity-90 cursor-pointer mx-auto flex gap-2 items-center text-[#CCA158]">
+                    <a download href={downloadUrl} className="absolute bottom-8 p-2 bg-[#EAEAEA] hover:opacity-90 cursor-pointer mx-auto flex gap-2 items-center text-[#CCA158]">
                         Baixar Imagem <Download />
                     </a>
                 
