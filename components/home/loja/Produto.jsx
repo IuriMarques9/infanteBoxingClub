2c33ac6adbba2ac8d42fa2e3a3ba14efa401f3d8
IuriMarques9 <iurimarcianom@hotmail.com>
2025-08-13 16:26:39 +0100

    shop images max w

--- components/home/loja/Produto.jsx
+++ components/home/loja/Produto.jsx
@@ -5,7 +5,7 @@ export default function Produto(props) {
     return (
             <div className="rounded-sm w-full h-full md:min-w-1/3 shadow-[inset_0px_0px_50px_35px_rgba(204,_161,_88,_100)] p-5">
                 {/* Product details will be displayed here */}
-                <Image src={props.url} alt={props.id} width={props.width} height={props.height} />
+                <Image className="max-w-80 place-self-center" src={props.url} alt={props.id} width={props.width} height={props.height} />
 
                 <h4 className="!text-white">{props.context?.caption || "Sem titulo"}</h4>
                 <p className="!text-white">{props.context?.Preço || "0,00€"}</p>
