0343ec553e14c12f967165898cb5f123d96c327d
IuriMarques9 <iurimarcianom@hotmail.com>
2025-08-21 14:36:24 +0100

    almost finished

--- components/home/eventos/Colecao.jsx
+++ components/home/eventos/Colecao.jsx
@@ -1,12 +1,15 @@
 "use client"
 import { X } from "lucide-react";
 import { createPortal } from "react-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
+import ModalImagem from "./ModalImagem";
 export default function Colecao({imagens, onClose}) {
     
-    console.log(imagens)
 
+    const [isImageOpen, setIsImageOpen] = useState(false);
+    const [selectedImage, setSelectedImage] = useState(null);
+    
     useEffect(() => { //Evita o scroll
         document.body.style.overflow = "hidden";
         return () => {
@@ -15,14 +18,22 @@ export default function Colecao({imagens, onClose}) {
     }, []);
 
     return createPortal(
-        <div className="md:py-20 pt-10 pb-40 sm:px-40 bg-black/70 backdrop-blur-xs fixed top-0 left-0 z-99 w-full h-screen">
-            <div className="mb-5 overflow-auto w-full h-full mx-auto grid content-start md:content-center gap-5 grid-cols-[repeat(auto-fit,minmax(250px,1fr))]">
-                {imagens.map((img) => {
-                    <Image src={img.url} height={img.height} width={img.width}/>
-                })}
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/70 p-16">
+            <X onClick={onClose} className="absolute top-6 right-6 hover:cursor-pointer hover:scale-110" width="25" height="25" fill="transparent" stroke="#837d7dff" />							
+            
+            <div className="relative w-full max-w-2xl overflow-y-auto max-h-[90vh] overflow-x-hidden">                
+            
+                <div className="grid items-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                    {imagens.map((img) => (
+                        <div key={img.id}>
+                            <Image className="hover:scale-105 hover:grayscale-40 hover:cursor-pointer" onClick={() => {setIsImageOpen(true); setSelectedImage(img)}} src={img.url} height={img.height} width={img.width} alt="foto da coleção"/>
+                            
+                        </div>
+                    ))}
+                </div>
             </div>
-
-            <X onClick={onClose} className="flex-none hover:cursor-pointer hover:scale-110" width="25" height="25" fill="transparent" stroke="#837d7dff" />							
+            {/*Modal da imagem selecionada */}
+            {isImageOpen && <ModalImagem imagem={selectedImage} onClose={() => setIsImageOpen(false)}/>}
         </div>,
         document.body
     );
---
+++ components/home/eventos/ModalImagem.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import { X, Download } from "lucide-react";
+export default function ModalImagem({imagem, onClose}) {
+    
+    const [isLandscape, setIsLandscape] = useState(false);
+        
+    const downloadUrl = imagem.url.replace(`/upload/`, `/upload/fl_attachment:${imagem.id}/`);
+
+    useEffect(() => {
+        if (imagem.width && imagem.height) {
+        setIsLandscape(imagem.width > imagem.height);
+        }
+    }, [imagem.width, imagem.height]);
+    
+
+    return (
+        <div className="fixed top-0 right-0 w-full h-screen bg-black/80 z-99 flex flex-col justify-center items-center">
+            <Image className={`object-contain h-full ${isLandscape ? "rotate-90 md:rotate-0" : ""} `} src={imagem.url} width={imagem.width} height={imagem.height} alt={imagem.id}/>
+            
+            <X onClick={onClose} stroke="#837d7dff" className="absolute hover:scale-110 top-4 right-4 hover:cursor-pointer" />
+
+            <a download href={downloadUrl} title="Baixar Fotografia" className="absolute top-4 left-4 p-2 bg-white rounded-full hover:opacity-80 cursor-pointer mx-auto flex gap-2 items-center text-[#CCA158]">
+                <Download  size={24}/>
+            </a>
+        </div>
+    );
+}
