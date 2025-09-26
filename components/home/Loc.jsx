8ccfddb897155ae2e9648a4a8bdadc75e9e057c1
IuriMarques9 <iurimarcianom@hotmail.com>
2025-09-27 00:44:06 +0100

    Atualiza endereço na seção de localização para Rua Dâmaso da Encarnação Nº4

--- components/home/Loc.jsx
+++ components/home/Loc.jsx
@@ -7,7 +7,7 @@ export default function Loc() {
                 <div className="max-w-[1800px] mx-auto w-full">
                     <iframe className="h-fit min-h-50 w-full" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d269.15028295734277!2d-7.846227670943275!3d37.033358964631375!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-PT!2spt!4v1747145908241!5m2!1spt-PT!2spt" width="600" height="450" style={{ border:"0" }} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
                     
-                    <h6 className="text-white">Rua Dâmaso da Encarnação Nº5, 8700-247 Olhão</h6>
+                    <h6 className="text-white">Rua Dâmaso da Encarnação Nº4, 8700-247 Olhão</h6>
                 </div>
         </section>
     );
